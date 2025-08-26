a5818aff934d9f3789d912accb3436c4cd84b032
yumi <keuzour3416@gmail.com>
2025-08-27 06:13:46 +0900

    comment function completed

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
+use App\Models\Comment;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(CommentRequest $request, Product $product)
+    {
+        Comment::create([
+            'user_id' => Auth::id(),
+            'product_id' => $product->product_id,
+            'content' => $request->input('content'),
+        ]);
+
+        return back();
+    }
 }
